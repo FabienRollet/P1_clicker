25d896281666c2318d28e19249f49fd9f77505e8
PAUL BOURY <paulboury17@gmail.com>
2024-03-14 12:12:51 +0100

    Added popup feature

---
+++ script.js
@@ -0,0 +1,8 @@
+function togglePopup1 () {
+    let popup = document.querySelector(".how-to-play");
+    popup.classList.toggle("open");
+}
+function togglePopup2 () {
+    let popup = document.querySelector(".who-are-we");
+    popup.classList.toggle("open");
+}
