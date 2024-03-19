47034405170a8e500487d3accbde3569f1a49c35
kevin belin <sorrento974@gmail.com>
2024-03-19 13:50:18 +0100

    ajout carrousselle pub

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function togglePopup2 () {
     popup.classList.toggle("open");
 }
 
-var nombreClics = 0;
+let nombreClics = 0;
 
 function comptage() {
   nombreClics++;
@@ -15,3 +15,33 @@ function comptage() {
 }
 
 document.getElementById("boutonClic").addEventListener("click", comptage);
+
+// debut carroussel
+window.onload = function () { 
+	let slides = 
+		document.getElementsByClassName('carousel-item'); 
+
+	function addActive(slide) { 
+		slide.classList.add('active'); 
+	} 
+
+	function removeActive(slide) { 
+		slide.classList.remove('active'); 
+	} 
+
+	addActive(slides[0]); 
+	setInterval(function () { 
+		for (let i = 0; i < slides.length; i++) { 
+			if (i + 1 == slides.length) { 
+				addActive(slides[0]); 
+				setTimeout(removeActive, 350, slides[i]); 
+				break; 
+			} 
+			if (slides[i].classList.contains('active')) { 
+				setTimeout(removeActive, 350, slides[i]); 
+				addActive(slides[i + 1]); 
+				break; 
+			} 
+		} 
+	}, 1500); 
+};
