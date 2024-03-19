798dfcd3607d954e672d40328f14c200034bb30a
pjroggy <pierrejeanroggy@gmail.com>
2024-03-19 16:49:47 +0100

    add setInterval to autocliker1 and add substract of price on numberClicks

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-function togglePopup0 () {
+function togglePopup0() {
   let popup = document.querySelector(".navbar");
   popup.classList.toggle("open");
 }
 
-function togglePopup1 () {
-    let popup = document.querySelector(".how-to-play");
-    popup.classList.toggle("open");
+function togglePopup1() {
+  let popup = document.querySelector(".how-to-play");
+  popup.classList.toggle("open");
 }
-function togglePopup2 () {
-    let popup = document.querySelector(".who-are-we");
-    popup.classList.toggle("open");
+function togglePopup2() {
+  let popup = document.querySelector(".who-are-we");
+  popup.classList.toggle("open");
 }
 /*clic croissant*/
-let nombreClics = 0;
+let numberClicks = 0;
 
 function comptage() {
-  nombreClics++;
-  document.getElementById("nombreClics").textContent   = nombreClics;
+  numberClicks++;
+  document.getElementById("numberClicks").textContent = numberClicks;
 }
 
 document.getElementById("boutonClic").addEventListener("click", comptage);
@@ -26,22 +26,20 @@ document.getElementById("boutonClic").addEventListener("click", comptage);
 
 let prodAutoClicker1 = 0;
 let lvlAutoclicker1 = 0;
-let costAutoclicker1 = 0;
+let costAutoclicker1 = 1;
 
-function upValueAutoClicker1(){
+function upValueAutoClicker1() {
   prodAutoClicker1++;
   lvlAutoclicker1++;
-  costAutoclicker1++;
+  document.getElementById("numberClicks").textContent = (numberClicks-= costAutoclicker1);
+  costAutoclicker1 += 2;
   document.getElementById("prodAutoClicker1").textContent = prodAutoClicker1;
   document.getElementById("lvlAutoclicker1").textContent = lvlAutoclicker1;
   document.getElementById("costAutoclicker1").textContent = costAutoclicker1;
 }
-
 document.getElementById("autoClicker1").addEventListener("click", upValueAutoClicker1);
 
 
-// function activeAutoClick(prodAutoclicker1){
-
-// }
-// let timerId = setTimeout(), 100;
-// onclick setTimout(comptage(valeur prodAutoclicker1), 100);
+setInterval(() => {
+    document.getElementById("numberClicks").textContent = numberClicks += prodAutoClicker1;
+}, 1000);
