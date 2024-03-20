92a62064434e2703ed029bb816b202f6e216166d
Rollet Fabien <fabrol47@gmail.com>
2024-03-20 08:58:34 +0100

    Revert "add setInterval to autocliker1 and add substract of price on numberClicks"
    This reverts commit 798dfcd3607d954e672d40328f14c200034bb30a.

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-function togglePopup0() {
+function togglePopup0 () {
   let popup = document.querySelector(".navbar");
   popup.classList.toggle("open");
 }
 
-function togglePopup1() {
-  let popup = document.querySelector(".how-to-play");
-  popup.classList.toggle("open");
+function togglePopup1 () {
+    let popup = document.querySelector(".how-to-play");
+    popup.classList.toggle("open");
 }
-function togglePopup2() {
-  let popup = document.querySelector(".who-are-we");
-  popup.classList.toggle("open");
+function togglePopup2 () {
+    let popup = document.querySelector(".who-are-we");
+    popup.classList.toggle("open");
 }
 
 
 /*clic croissant*/
-let numberClicks = 0;
+let nombreClics = 0;
 
 function comptage() {
   numberClicks++;
@@ -72,17 +72,17 @@ document.getElementById("upgradeImg1").addEventListener("click", increaseTheValu
 
 let prodAutoClicker1 = 0;
 let lvlAutoclicker1 = 0;
-let costAutoclicker1 = 1;
+let costAutoclicker1 = 0;
 
-function upValueAutoClicker1() {
+function upValueAutoClicker1(){
   prodAutoClicker1++;
   lvlAutoclicker1++;
-  document.getElementById("numberClicks").textContent = (numberClicks-= costAutoclicker1);
-  costAutoclicker1 += 2;
+  costAutoclicker1++;
   document.getElementById("prodAutoClicker1").textContent = prodAutoClicker1;
   document.getElementById("lvlAutoclicker1").textContent = lvlAutoclicker1;
   document.getElementById("costAutoclicker1").textContent = costAutoclicker1;
 }
+
 document.getElementById("autoClicker1").addEventListener("click", upValueAutoClicker1);
 
 setInterval(() => {
