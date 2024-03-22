aa55910d5e66803730ee25fcc87d44bbac32b60d
Rollet Fabien <fabrol47@gmail.com>
2024-03-22 10:05:29 +0100

    changed upgrade 1 effet

--- script.js
+++ script.js
@@ -235,7 +235,6 @@ facerelease.addEventListener("mouseup", facerelease);
 // upgrade debloquage compteur
 
 function addValueOfUpgrade1() {
-  numberClicks += 1000;
   twoClick=true;
   document.getElementById("numberClicks").textContent = numberClicks;
 }
